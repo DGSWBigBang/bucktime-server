ce3d0991fd9676dceef05793c48487c44712b78e
gwon11225 <gwon11225@gmail.com>
2023-09-06 19:11:17 +0900

    spring initializr

---
+++ src/main/java/com/bigbang/ducktime/DucktimeApplication.java
@@ -0,0 +1,13 @@
+package com.bigbang.ducktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucktimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucktimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigbang/ducktime/DucktimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbang.ducktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucktimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
