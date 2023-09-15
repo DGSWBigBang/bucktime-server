16aee9c0c6fffa96aa2eae3fc36d9ca5912c9536
gwon11225 <gwon11225@gmail.com>
2023-09-15 22:28:55 +0900

    yml파일 수정

---
+++ src/main/java/com/bigbang/bucktime/domain/order/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.bigbang.bucktime.domain.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+}
