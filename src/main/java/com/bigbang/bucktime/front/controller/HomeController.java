1021113e160be6e0f1f59d5c16e2ad5ff17239f6
Woogong <batang21@gmail.com>
2023-10-31 19:17:42 +0900

    front page 작성중

--- src/main/java/com/bigbang/bucktime/front/controller/HomeController.java
+++ src/main/java/com/bigbang/bucktime/front/controller/HomeController.java
@@ -2,13 +2,27 @@ package com.bigbang.bucktime.front.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/front")
 public class HomeController {
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@GetMapping("/user/{pageName}")
+	public String userPages(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@PathVariable("pageName") String pageName) {
+		return "user/" + pageName;
 	}
 	
 }
--- src/main/java/com/bigbang/bucktime/global/config/SecurityConfig.java
+++ src/main/java/com/bigbang/bucktime/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.bigbang.bucktime.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,12 +32,15 @@ public class SecurityConfig {
                         .configurationSource(corsConfigurationSource())
                 )
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
                 .logout(logout -> logout.logoutUrl("/user/logout").deleteCookies("accessToken").deleteCookies("refreshToken"))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
                         .requestMatchers("/notice/create", "/notice/modify", "/notice/delete").hasAuthority("ADMIN")
                         .requestMatchers("/desk/create", "/desk/modify", "/desk/delete", "/cafe/modify", "/cafe/show/owner", "/order/modify/completion", "/order/show/owner", "/menu/create", "/menu/modify", "/menu/delete").hasAuthority("OWNER")
                         .requestMatchers("/user/modify", "/user/show", "/user/delete", "/order/create", "/order/show/user", "/rez/modify/used", "/rez/create").hasAuthority("USER")
+                        .requestMatchers("/hello").hasAuthority("OWNER")
+                        
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
